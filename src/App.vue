b5a8ef07c6d26e878d536a15089b9d257494c023
Adulatian <jin.tian984@outlook.com>
2023-09-26 15:31:30 +0800

    hello

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+  <div id="app">
+    <h1>To-Do List</h1>
+    <ul>
+      <li>
+        <to-do-item label = "My todo list." :done="true"></to-do-item>
+      </li>
+    </ul>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  import ToDoItem from "./components/ToDoItem.vue";
+
+  export default {
+      name: "app",
+      components: {
+        ToDoItem,
+      },
+     
+  };
+
 </script>
 
 <style>
