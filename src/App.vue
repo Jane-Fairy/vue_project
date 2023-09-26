a863eff70854dbc767b8018b9bfdf48ee1e1a2ff
Adulatian <jin.tian984@outlook.com>
2023-09-26 17:58:42 +0800

    second

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
- 
   <div id="app">
-    <h1>To-Do List</h1>
+    <h1>My To-Do List</h1>
+    <to-do-form @todo-added="addToDo"></to-do-form>
     <ul>
-      <li>
-        <to-do-item label = "My todo list." :done="true"></to-do-item>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item
+          :label="item.label"
+          :done="item.done"
+          :id="item.id"></to-do-item>
       </li>
     </ul>
   </div>
-
 </template>
+
 
 <script>
 
   import ToDoItem from "./components/ToDoItem.vue";
+  import ToDoForm from "./components/ToDoForm.vue";
+  import uniqueId from "lodash.uniqueid";
 
   export default {
-      name: "app",
-      components: {
-        ToDoItem,
+    name: "app",
+    components: {
+      ToDoItem,
+      ToDoForm,
+    },
+    data() {
+      return {
+        ToDoItems: [
+          { id: uniqueId("todo-"), label: "Learn Vue", done: false },
+          {
+            id: uniqueId("todo-"),
+            label: "Create a Vue project with the CLI",
+            done: true,
+          },
+          { id: uniqueId("todo-"), label: "Have fun", done: true },
+          { id: uniqueId("todo-"), label: "Create a to-do list", done: false },
+        ],
+      };
+    },
+    methods: {
+      addToDo() {
+        console.log("To-do added");
       },
-     
+    },
   };
 
 </script>
