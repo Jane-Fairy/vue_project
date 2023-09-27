4f2f9ac184c0e2a97e172f6cc40a0c8d61755981
Adulatian <jin.tian984@outlook.com>
2023-09-27 11:42:59 +0800

    dsa.

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
   <div id="app">
     <h1>My To-Do List</h1>
     <to-do-form @todo-added="addToDo"></to-do-form>
+    <h2 id="list-summary">{{listSummary}}</h2>
     <ul aria-labelledby="list-summary" class="stack-large">
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item
           :label="item.label"
           :done="item.done"
-          :id="item.id"></to-do-item>
+          :id="item.id"
+          @checkbox-changed="updateDoneStatus(item.id)"
+          ></to-do-item>
       </li>
     </ul>
   </div>
@@ -27,6 +30,7 @@
       ToDoForm,
     },
     data() {
+      
       return {
         ToDoItems: [
           { id: uniqueId("todo-"), label: "Learn Vue", done: false },
@@ -44,7 +48,17 @@
       addToDo(toDoLabel) {
          this.ToDoItems.push({id:uniqueId('todo-'), label: toDoLabel, done: false});
       },
+      updateDoneStatus(toDoId) {
+        const toDoToUpdate = this.ToDoItems.find((item) => item.id === toDoId)
+        toDoToUpdate.done = !toDoToUpdate.done
+      }
     },
+    computed: {
+      listSummary() {
+        const numberFinishedItems = this.ToDoItems.filter((item) =>item.done).length
+        return `${numberFinishedItems} out of ${this.ToDoItems.length} items completed`
+      }
+    }
   };
 
 </script>
