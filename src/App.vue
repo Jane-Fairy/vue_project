73fd01afcd527f12e00cade6e595b07e5899fd59
Adulatian <jin.tian984@outlook.com>
2023-09-27 17:10:28 +0800

    finish

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <h1>My To-Do List</h1>
     <to-do-form @todo-added="addToDo"></to-do-form>
-    <h2 id="list-summary">{{listSummary}}</h2>
+    <h2 id="list-summary"  ref="listSummary" tabindex="-1">{{listSummary}}</h2>
     <ul aria-labelledby="list-summary" class="stack-large">
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item
@@ -10,6 +10,8 @@
           :done="item.done"
           :id="item.id"
           @checkbox-changed="updateDoneStatus(item.id)"
+          @item-deleted="deleteToDo(item.id)"
+          @item-edited="editToDo(item.id, $event)"
           ></to-do-item>
       </li>
     </ul>
@@ -33,14 +35,7 @@
       
       return {
         ToDoItems: [
-          { id: uniqueId("todo-"), label: "Learn Vue", done: false },
-          {
-            id: uniqueId("todo-"),
-            label: "Create a Vue project with the CLI",
-            done: true,
-          },
-          { id: uniqueId("todo-"), label: "Have fun", done: true },
-          { id: uniqueId("todo-"), label: "Create a to-do list", done: false },
+         
         ],
       };
     },
@@ -51,6 +46,15 @@
       updateDoneStatus(toDoId) {
         const toDoToUpdate = this.ToDoItems.find((item) => item.id === toDoId)
         toDoToUpdate.done = !toDoToUpdate.done
+      },
+      deleteToDo(toDoId) {
+        const itemIndex = this.ToDoItems.findIndex((item) => item.id === toDoId);
+        this.ToDoItems.splice(itemIndex, 1);
+        this.$refs.listSummary.focus();
+      },
+      editToDo(toDoId, newLabel) {
+        const toDoToEdit = this.ToDoItems.find((item) => item.id === toDoId);
+        toDoToEdit.label = newLabel;
       }
     },
     computed: {
--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,26 +1,88 @@
 <template>
-  <div class="custom-checkbox">
-    <input type="checkbox" :id="id" :checked="isDone"  @change="$emit('checkbox-changed')" />
-    <label :for="id"> {{label}} </label>
+  <div class="stack-small"  v-if="!isEditing">
+    <div class="custom-checkbox">
+      <input
+        type="checkbox"
+        class="checkbox"
+        :id="id"
+        :checked="isDone"
+        @change="$emit('checkbox-changed')" />
+      <label :for="id" class="checkbox-label">{{label}}</label>
+    </div>
+    <div class="btn-group">
+      <button 
+        type="button" 
+        class="btn" 
+        ref="editButton"
+        @click="toggleToItemEditForm">
+        Edit 
+        <span class="visually-hidden">{{label}}</span>
+      </button>
+      <button type="button" class="btn btn__danger" @click="deleteToDo">
+        Delete <span class="visually-hidden">{{label}}</span>
+      </button>
+    </div>
   </div>
+  <to-do-item-edit-form
+    v-else
+    :id="id"
+    :label="label"
+    @item-edited="itemEdited"
+    @edit-cancelled="editCancelled">
+  </to-do-item-edit-form>
 </template>
 
 
 <script scoped>
  import uniqueId from "lodash.uniqueid";
 
-
+ import ToDoItemEditForm from "./ToDoItemEditForm";
  export default {
+
+    components: {
+      ToDoItemEditForm
+    },
+
     props: {
       label: { required: true, type: String },
       done: { default: false, type: Boolean }
     },
     data() {
       return {
-        isDone: this.done,
         id: uniqueId("todo-"),
+        isEditing: false
       };
     },
+    computed: {
+      isDone() {
+        return this.done;
+      }
+    },
+    methods: {
+      focusOnEditButton() {
+         this.$nextTick(() => {
+          const editButtonRef = this.$refs.editButton;
+          editButtonRef.focus();
+        });
+      },
+
+      deleteToDo() {
+        this.$emit('item-deleted');
+      },
+      toggleToItemEditForm() {
+        console.log(this.$refs.editButton);
+        this.isEditing = true;
+      },
+      itemEdited(newItemName) {
+        this.$emit('item-edited', newItemName);
+        this.isEditing = false;
+        this.focusOnEditButton();
+      },
+      editCancelled() {
+        this.isEditing = false;
+        this.focusOnEditButton();
+      }
+    }
   };
 </script>
 
